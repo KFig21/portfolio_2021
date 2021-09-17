aa494e24fbec09304a1c1e8bcd0b76947e975723
Kevin <kfig21@gmail.com>
2021-09-17 01:39:21 -0400

    icons for portfolio section

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -21,79 +21,81 @@ export default function Modal({ project, setShowModal }) {
   return (
     <div className="modal">
       <div className="modal-container" key={project.id}>
-        <div className="modal-item">
+        <div className="modal-wrapper">
           <div className="favicon-container">
             <img src={project.favicon} alt="favicon" className="favicon" />
           </div>
-          <button className="closeX" onClick={() => setShowModal(false)}>
-            <div>
-              <span>x</span>
-            </div>
-          </button>
-          <div className="left">
-            <div className="left-container">
-              {/* desktop icons */}
-              <div className="icons-container desktop">
-                {project.icons.map((tech) => (
-                  <div key={tech.id}>
-                    <div className={`img-container ${tech.color}-color-bg`}>
-                      <img src={tech.icon} alt="" />
-                      <span className={`stack ${tech.color}-color-text`}>
-                        {tech.color}
-                      </span>
+          <div className="modal-item">
+            <button className="closeX" onClick={() => setShowModal(false)}>
+              <div>
+                <span>x</span>
+              </div>
+            </button>
+            <div className="left">
+              <div className="left-container">
+                {/* desktop icons */}
+                <div className="icons-container desktop">
+                  {project.icons.map((tech) => (
+                    <div key={tech.id}>
+                      <div className={`img-container ${tech.color}-color-bg`}>
+                        <img src={tech.icon} alt="" />
+                        <span className={`stack ${tech.color}-color-text`}>
+                          {tech.color}
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                ))}
-              </div>
-              <h2>{project.title}</h2>
-              <p>{project.desc}</p>
-              {/* mobile icons */}
-              <div className="icons-container mobile">
-                {project.icons.map((tech) => (
-                  <div key={tech.id}>
-                    <div className={`img-container ${tech.color}-color-bg`}>
-                      <img src={tech.icon} alt="" />
-                      <span className={`stack ${tech.color}-color-text`}>
-                        {tech.color}
-                      </span>
+                  ))}
+                </div>
+                <h2>{project.title}</h2>
+                <p>{project.desc}</p>
+                {/* mobile icons */}
+                <div className="icons-container mobile">
+                  {project.icons.map((tech) => (
+                    <div key={tech.id}>
+                      <div className={`img-container ${tech.color}-color-bg`}>
+                        <img src={tech.icon} alt="" />
+                        <span className={`stack ${tech.color}-color-text`}>
+                          {tech.color}
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                ))}
-              </div>
-              <div className="details-container ">
-                <a
-                  href={project.repository}
-                  className="option"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <CodeIcon style={{ margin: " 0px 5px" }} />
-                  Repo
-                </a>
-                <a
-                  href={project.live}
-                  className="option"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <LanguageIcon style={{ margin: " 0px 5px" }} />
-                  Live
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="right">
-            {project.imgs.map((img) => {
-              const { id, src } = img;
-
-              return (
-                <div className="img-container">
-                  <a href={src} target="_blank" rel="noopener noreferrer">
-                    <img id={id} src={src} alt="" />
+                  ))}
+                </div>
+                <div className="details-container ">
+                  <a
+                    href={project.repository}
+                    className="option"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <CodeIcon style={{ margin: " 0px 5px" }} />
+                    Repo
+                  </a>
+                  <a
+                    href={project.live}
+                    className="option"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <LanguageIcon style={{ margin: " 0px 5px" }} />
+                    Live
                   </a>
                 </div>
-              );
-            })}
+              </div>
+            </div>
+            <div className="right">
+              {project.imgs.map((img) => {
+                const { id, src } = img;
+
+                return (
+                  <div className="img-container">
+                    <a href={src} target="_blank" rel="noopener noreferrer">
+                      <img id={id} src={src} alt="" />
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
